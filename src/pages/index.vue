d1fe005fab33bba5133714889447d3ac41b51609
Andrew Crescencio <68707868+AndrewCrescencio@users.noreply.github.com>
2024-09-20 05:15:01 -0300

    refactor: remove unnecessary code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,8 +3,6 @@ import { legendsMock } from '../mock/index.ts'
 
 const legends = ref([...legendsMock])
 </script>
-ref([...legendsMock])
-
 <template>
   <AppHero />
   <SectionCtaLegends :legends="legends" />
